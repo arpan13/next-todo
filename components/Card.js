fec57bf07b042cfb9faf76f6f3e9665902327225
Arpan Kumar Pal <arpankumarpal728@gmail.com>
2022-06-05 01:42:09 +0530

    started todo app with next.js

---
+++ components/Card.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import styles from './Card.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+    faTrash
+} from "@fortawesome/free-solid-svg-icons";
+export default function Card({ todo }) {
+    const [checked, setChecked] = useState(false);
+
+    function onClickCheckBox() {
+        setChecked(!checked)
+
+    }
+
+
+    return (
+        <div className={styles.card}>
+            <div>
+                <input type="checkbox" name="" id="check" onClick={onClickCheckBox} />
+            </div>
+
+            <h3>{todo}</h3>
+            <div>
+                <FontAwesomeIcon icon={faTrash} />
+            </div>
+
+
+        </div>
+    )
+}
